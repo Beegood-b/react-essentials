2109bf16be6fbc1bffd371ed0a7aaab28e58e086
Artjoms Bugajenkovs <k3nnel123@gmail.com>
2024-01-31 21:56:42 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Beegood-b/First-React-App',
+  base: "/First-React-App/",
 });
 
 
